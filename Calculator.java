c87f132d5541cc1ae44e89f21b9ee6000e49a2ab
sammie333 <115142583+sammie333@users.noreply.github.com>
2024-08-05 16:54:01 +0800

    add java file

---
+++ Calculator.java
@@ -0,0 +1,28 @@
+//test
+//test_dony
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    // 乘法
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+    // hello world
+    public String greeting() {
+        return "Hello Wrold";
+    }
+
+     // hello world
+    public String greeting() {
+        return "i'm sammie";
+    }
+}
